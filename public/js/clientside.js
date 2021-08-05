33e6f8caf098c5ea9fd3dd68e6273723d30d2504
Yashkanekar <kanekaryash.11@gmail.com>
2021-08-05 21:22:56 +0530

    setup app for heroku

--- public/js/clientside.js
+++ public/js/clientside.js
@@ -9,7 +9,7 @@ form.addEventListener('submit', (e) => {
 
     messageOne.textContent = 'Getting Weather info...'
     messageTwo.textContent = ''
-    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+    fetch(`/weather?address=${location}`).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error
--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const forecast = require('./utils/forecast')
 
 const app = express()
 
+const port = process.env.PORT || 3000
+
 //define paths for express config
 const publicDirectoryPath =  path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname,'../templates/views')
@@ -93,6 +95,6 @@ app.get('*', (req,res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is running at port 3000!')
+app.listen(port, () => {
+    console.log(`Server is running at port ${port} `)
 })
